f5a26363d4628ae889b4332933f5070ffc3e51c4
Sam Sully <sam@planetaryprocessing.io>
2023-08-17 18:00:00 +0100

    back to raw lua

--- luapb.c
+++ luapb.c
@@ -4,7 +4,7 @@
 
 typedef struct {
   unsigned char *arr;
-  int length;
+  int32_t length;
 } byte_array;
 
 typedef struct {
@@ -12,7 +12,18 @@ typedef struct {
   uint64_t fieldnum;
 } tag;
 
-int32_t getVarintLength(unsigned char bytes[], int num) {
+typedef struct {
+  tag tag;
+  byte_array bytes;
+  int32_t length;
+} record;
+
+typedef struct {
+  record *records;
+  int32_t num;
+} message;
+
+int getVarintLength(unsigned char bytes[], int num) {
   for (int l = 0; l < num; l++) {
     if (bytes[l] >> 7 == 0) {
       return l+1;
@@ -59,4 +70,38 @@ tag decodeTag(uint64_t in) {
 
 uint64_t encodeTag(uint64_t fieldnum, uint64_t wiretype) {
   return (fieldnum << 3) | wiretype;
+}
+
+record decodeRecord(unsigned char bytes[], int num) {
+  record r;
+  int l = getVarintLength(bytes, num);
+  uint64_t ti = decodeVarint(bytes, l);
+  tag t = decodeTag(ti);
+  r.tag = t;
+  r.length = l;
+  int32_t size = 0;
+  switch (t.wiretype) {
+    case 0:
+      size = getVarintLength(bytes+r.length, num-r.length);
+      break;
+    case 1:
+      size = 8;
+      break;
+    case 2:
+      int vil = getVarintLength(bytes+r.length, num-r.length);
+      int32_t ti = (int32_t)decodeVarint(bytes+r.length, vil);
+      r.length += vil;
+      size = ti;
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      size = 4;
+      break;
+  }
+  r.bytes = (byte_array){bytes+r.length, size};
+  r.length +=size;
+  return r;
 }
--- luapb.lua
+++ luapb.lua
@@ -7,7 +7,7 @@ local pp = ffi.load("./libluapb.so")
 ffi.cdef([[
   typedef struct {
     unsigned char *arr;
-    int length;
+    int32_t length;
   } byte_array;
 
   typedef struct {
@@ -15,26 +15,46 @@ ffi.cdef([[
     uint64_t fieldnum;
   } tag;
 
-  int32_t getVarintLength(unsigned char bytes[], int num);
+  typedef struct {
+    tag tag;
+    byte_array bytes;
+    int32_t length;
+  } record;
+
+  typedef struct {
+    record *records;
+    int32_t num;
+  } message;
+
+  int getVarintLength(unsigned char bytes[], int num);
 
   byte_array encodeVarint(uint64_t v);
   uint64_t decodeVarint(unsigned char bytes[], int num);
 
   uint64_t encodeTag(uint64_t wiretype, uint64_t fieldnum);
   tag decodeTag(uint64_t in);
+
+  record decodeRecord(unsigned char bytes[], int num);
 ]])
 
--- test decodeVarint
+print("decodeVarint")
 print(tonumber(pp.decodeVarint(ffi.new("unsigned char[2]", {0x96, 0x01}), 2))) -- should be 150
 print(tonumber(ffi.cast("int64_t", pp.decodeVarint(ffi.new("unsigned char[10]", {0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01}), 10)))) -- should be -2 (casted for easy reading)
 
--- test encodeVarint
+print("\nencodeVarint")
 printl(arrToTable(pp.encodeVarint(150))) -- should be {96, 01}
 printl(arrToTable(pp.encodeVarint(0xFFFFFFFFFFFFFFFEULL))) -- should be {fe, ff, ff, ff, ff, ff, ff, ff, ff, 01}
 
--- test decodeTag
+print("\ndecodeTag")
 local tag = pp.decodeTag(0x08)
 print(tag.fieldnum, tag.wiretype) -- should be 1, 0
 
--- test encodeTag
+print("\nencodeTag")
 print(pp.encodeTag(1, 0)) -- should be 8
+
+print("\ndecodeRecord")
+local rec = pp.decodeRecord(ffi.new("unsigned char[9]", {0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67}), 9)
+print(rec.tag.fieldnum, rec.tag.wiretype) -- should be 2, 2
+print(rec.length) -- should be 9
+print(rec.bytes.length) -- should be 7
+printl(arrToTable(rec.bytes)) -- should be {74, 65, 73, 74, 69, 6e, 67}
